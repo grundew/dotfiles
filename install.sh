79e6ed8bb01bdb236b967127a78b15bf84749fb8
Grunde Waag <grunde@dolphitech.com>
2021-01-12 15:52:45 +0100

    installer bug

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ plugindir=~/.vim/pack/bundle/start
 git clone --recurse-submodules -j8 git@github.com:grundew/dotfiles.git ~/dotfiles
 
 mkdir -p $plugindir
-ln -s ~/dotfiles/vimrc ./vim/vimrc
+ln -s ~/dotfiles/vimrc ~/.vim/vimrc
 
 for file in ~/dotfiles/*/ ; do ln -s $file $plugindir ; done
