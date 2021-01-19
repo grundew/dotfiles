2195c95f90f60618649d8ea98470a711aeda101f
Grunde Waag <grunde@dolphitech.com>
2021-01-19 22:13:48 +0100

    Added tmux plugin manager

--- install.sh
+++ install.sh
@@ -1,17 +1,16 @@
 #! /usr/bin/zsh
-
-vimplugindir=~/.vim/pack/bundle/start
-
 git clone --recurse-submodules -j8 git@github.com:grundew/dotfiles.git ~/dotfiles
 
+# vim config
+vimplugindir=~/.vim/pack/bundle/start
 mkdir -p $vimplugindir
 ln -s ~/dotfiles/vimrc ~/.vim/vimrc
-
-for file in ~/dotfiles/vimplugins/*/ ; do ln -s $file $plugindir ; done
+for file in ~/dotfiles/vimplugins/*/ ; do ln -s $file $vimplugindir ; done
 
 # tmux conf
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-
-
+tmuxplugindir=~/.tmux/plugins
+mkdir -p $tmuxplugindir
+for file in ~/dotfiles/tmuxplugins/*/ ; do ln -s $file $tmuxplugindir ; done
 
 # Conky
